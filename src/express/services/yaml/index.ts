e7fb3c59a0f766539c1e02393234f6dcbe4347b4
Avi Levy <avicorp@gmail.com>
2021-07-19 22:25:16 +0300

    setting mongodb connection from env, in case of missing env variable error will be raised.

--- src/express/services/yaml/index.ts
+++ src/express/services/yaml/index.ts
@@ -1,4 +1,5 @@
 import { injectable } from 'inversify';
+import { cleanEnv, str, email, json, port } from 'envalid';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as _ from 'lodash';
@@ -7,62 +8,89 @@ import * as yaml from 'js-yaml';
 @injectable()
 export class Config {
 
-  private config: YamlConfig;
-  private filePath: string;
+    private config: YamlConfig;
+    private filePath: string;
 
-  public constructor(filePath: string) {
-    this.filePath = filePath;
-    this.config = <YamlConfig>{};
-  }
-
-  private async readConfig(): Promise<void> {
-    // attempt to read the yml file and potentially throw IO Exception
-    const content = await new Promise<string>((resolve, reject) => {
-      fs.readFile(path.resolve(process.cwd(), this.filePath), 'utf8', (err, content) => {
-        if (err) {
-          return reject(err);
-        }
-        return resolve(content);
-      });
-    });
-    this.config = yaml.safeLoad(content);
-  }
-
-  public async getConfig(): Promise<YamlConfig> {
-    if (_.isEmpty(this.config)) {
-      await this.readConfig();
+    public constructor(filePath: string) {
+        this.filePath = filePath;
+        this.config = <YamlConfig>{};
     }
-    return this.config;
-  }
+
+    private async readConfig(): Promise<void> {
+        // attempt to read the yml file and potentially throw IO Exception
+        const content = await new Promise<string>((resolve, reject) => {
+            fs.readFile(path.resolve(process.cwd(), this.filePath), 'utf8', (err, content) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(content);
+            });
+        });
+        this.config = <YamlConfig> yaml.safeLoad(content);
+    }
+
+    public async getConfig(): Promise<YamlConfig> {
+        if (_.isEmpty(this.config)) {
+            await this.readConfig();
+        }
+        this.readENV();
+        return this.config;
+    }
+
+    private readENV() {
+        const env = this.validENV();
+        const MONGO_PORT = env.MONGO_PORT;
+        this.config.mongodb.port = Number(MONGO_PORT);
+        const MONGO_URL = env.MONGO_URL;
+        this.config.mongodb.host = String(MONGO_URL);
+        const MONGO_USER_NAME = env.MONGO_USER_NAME;
+        this.config.mongodb.username = String(MONGO_USER_NAME);
+        const MONGO_PASSWORD = env.MONGO_PASSWORD;
+        this.config.mongodb.password = String(MONGO_PASSWORD);
+        const PORT = env.SERVER_PORT;
+        this.config.server_port = Number(PORT);
+    }
+    private validENV(): any{
+        const env = cleanEnv(process.env, {
+            MONGO_URL: str(),
+            MONGO_USER_NAME: str(),
+            MONGO_PASSWORD: str(),
+            NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
+            MONGO_PORT: port({ default: 443 }),
+            SERVER_PORT: port ({ default: 8080 })
+        })
+        return env;
+    }
+
 
 }
 
 export interface YamlConfig {
-  jwt: JwtConfig,
-  mailer: MailerConfig,
-  mongodb: MongoConfig,
+    jwt: JwtConfig,
+    mailer: MailerConfig,
+    mongodb: MongoConfig,
+    server_port: number
 }
 
 export interface JwtConfig {
-  secret: string,
-  expireTime: number,
-  algorithem: 'HS256' | 'HS384' | 'HS512' | 'RS256' | 'RS384' | 'RS512' | 'ES256' | 'ES384' | 'ES512' | 'PS256' | 'PS384' | 'PS512' | 'none' | undefined,
+    secret: string,
+    expireTime: number,
+    algorithem: 'HS256' | 'HS384' | 'HS512' | 'RS256' | 'RS384' | 'RS512' | 'ES256' | 'ES384' | 'ES512' | 'PS256' | 'PS384' | 'PS512' | 'none' | undefined,
 }
 
 export interface MailerConfig {
-  host: string,
-  port: number,
-  username: string,
-  password: string,
-  fromAddress: string,
-  sendEmail: boolean,
+    host: string,
+    port: number,
+    username: string,
+    password: string,
+    fromAddress: string,
+    sendEmail: boolean,
 }
 
 export interface MongoConfig {
-  host: string,
-  port: number,
-  username?: string,
-  password?: string,
-  dbName: string,
-  replicaSetName?: string
+    host: string,
+    port: number,
+    username?: string,
+    password?: string,
+    replicaSetName?: string
 }
